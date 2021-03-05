3483e851858f64f71e7b5179983c46ea32842687
Sam MacPherson <xylom44@gmail.com>
2021-03-05 08:28:45 -0500

    maintain original copyright; newline at eof

--- contracts/l2/dss/dai.sol
+++ contracts/l2/dss/dai.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
+// Copyright (C) 2017, 2018, 2019 dbrock, rain, mrchico
 // Copyright (C) 2021 MakerDAO
 
 // This program is free software: you can redistribute it and/or modify
