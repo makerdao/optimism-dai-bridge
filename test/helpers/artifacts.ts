cb8de2f10b8e17d4cfd116b6dadf87b1de74e602
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-03-05 13:33:51 +0100

    Cleanup

--- test/helpers/artifacts.ts
+++ test/helpers/artifacts.ts
@@ -2,24 +2,7 @@ import { getContractDefinition } from '@eth-optimism/contracts'
 
 export const artifacts = {
   l1: {
-    // token: require('../../artifacts/contracts/ERC20.sol/ERC20.json'),
-    // tokenDeposit: require('../../artifacts/contracts/L1ERC20Deposit.sol/L1ERC20Deposit.json'),
     canonicalTxChain: getContractDefinition('OVM_CanonicalTransactionChain'),
     stateCommitmentChain: getContractDefinition('OVM_StateCommitmentChain'),
   },
-  // l2: {
-  //   dai: {
-  //     abi: JSON.parse(readFileSync(join(__dirname, '../../artifacts-l2/__contracts-l2_dss_dai_sol_Dai.abi'), 'utf-8')),
-  //     bytecode: readFileSync(join(__dirname, '../../artifacts-l2/__contracts-l2_dss_dai_sol_Dai.bin'), 'utf-8'),
-  //   },
-  //   minter: {
-  //     abi: JSON.parse(
-  //       readFileSync(join(__dirname, '../../artifacts-l2/__contracts-l2_L2ERC20Minter_sol_L2ERC20Minter.abi'), 'utf-8'),
-  //     ),
-  //     bytecode: readFileSync(
-  //       join(__dirname, '../../artifacts-l2/__contracts-l2_L2ERC20Minter_sol_L2ERC20Minter.bin'),
-  //       'utf-8',
-  //     ),
-  //   },
-  // },
 }
