ce218c9cc22c591f4084e04ffa3cca0169269d43
Sam MacPherson <xylom44@gmail.com>
2021-03-08 12:26:40 -0500

    fix dai unit tests

--- contracts/l2/dai.sol
+++ contracts/l2/dai.sol
@@ -134,7 +134,6 @@ contract Dai {
   // --- Approve by signature ---
   function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
     require(block.timestamp <= deadline, "Dai/permit-expired");
-    require(owner != address(0), "Dai/permit-expired");
 
     bytes32 digest =
       keccak256(abi.encodePacked(
--- test/l2/dai.ts
+++ test/l2/dai.ts
@@ -15,7 +15,7 @@ describe('Counter', () => {
   beforeEach(async () => {
     const [deployer, user1, user2, user3] = await ethers.getSigners()
     signers = { deployer, user1, user2, user3 }
-    const daiFactory = await ethers.getContractFactory('Dai', signers[0])
+    const daiFactory = await ethers.getContractFactory('Dai', deployer)
     dai = await daiFactory.deploy()
   })
 
@@ -44,41 +44,41 @@ describe('Counter', () => {
 
       it('transfers dai using transferFrom', async () => {
         const balanceBefore = await dai.balanceOf(signers.user2.address)
-        await dai.transferFrom(signers.user1, signers.user2, 1, { from: signers.user1 })
-        const balanceAfter = await dai.balanceOf(signers.user2)
-        balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+        await dai.connect(signers.user1).transferFrom(signers.user1.address, signers.user2.address, 1)
+        const balanceAfter = await dai.balanceOf(signers.user2.address)
+        balanceAfter.toString().should.equal(balanceBefore.add(1).toString())
       })
 
       it('should not transfer beyond balance', async () => {
-        await expectRevert(dai.transfer(signers.user2, 100, { from: signers.user1 }))
-        await expectRevert(dai.transferFrom(signers.user1, signers.user2, 100, { from: signers.user1 }))
+        await expectRevert.unspecified(dai.connect(signers.user1).transfer(signers.user2.address, 100))
+        await expectRevert.unspecified(dai.connect(signers.user1).transferFrom(signers.user1.address, signers.user2.address, 100))
       })
 
       it('approves to increase allowance', async () => {
-        const allowanceBefore = await dai.allowance(signers.user1, signers.user2)
-        await dai.approve(signers.user2, 1, { from: signers.user1 })
-        const allowanceAfter = await dai.allowance(signers.user1, signers.user2)
-        allowanceAfter.toString().should.equal(allowanceBefore.add(new BN('1')).toString())
+        const allowanceBefore = await dai.allowance(signers.user1.address, signers.user2.address)
+        await dai.connect(signers.user1).approve(signers.user2.address, 1)
+        const allowanceAfter = await dai.allowance(signers.user1.address, signers.user2.address)
+        allowanceAfter.toString().should.equal(allowanceBefore.add(1).toString())
       })
 
       it('approves to increase allowance with permit', async () => {
         const permitResult = await signERC2612Permit(
           web3.currentProvider,
           dai.address,
-          signers.user1,
-          signers.user2,
+          signers.user1.address,
+          signers.user2.address,
           '1',
         )
         await dai.permit(
-          signers.user1,
-          signers.user2,
+          signers.user1.address,
+          signers.user2.address,
           '1',
           permitResult.deadline,
           permitResult.v,
           permitResult.r,
           permitResult.s,
         )
-        const allowanceAfter = await dai.allowance(signers.user1, signers.user2)
+        const allowanceAfter = await dai.allowance(signers.user1.address, signers.user2.address)
         allowanceAfter.toString().should.equal('1')
       })
 
@@ -86,13 +86,13 @@ describe('Counter', () => {
         const permitResult = await signERC2612Permit(
           web3.currentProvider,
           dai.address,
-          signers.user1,
-          signers.user2,
+          signers.user1.address,
+          signers.user2.address,
           '1',
         )
         await expectRevert(
-          dai.permit(signers.user1, signers.user2, '1', 0, permitResult.v, permitResult.r, permitResult.s),
-          'WETH: Expired permit',
+          dai.permit(signers.user1.address, signers.user2.address, '1', 0, permitResult.v, permitResult.r, permitResult.s),
+          'Dai/permit-expired',
         )
       })
 
@@ -100,55 +100,49 @@ describe('Counter', () => {
         const permitResult = await signERC2612Permit(
           web3.currentProvider,
           dai.address,
-          signers.user1,
-          signers.user2,
+          signers.user1.address,
+          signers.user2.address,
           '1',
         )
         await expectRevert(
           dai.permit(
-            signers.user1,
-            signers.user2,
+            signers.user1.address,
+            signers.user2.address,
             '2',
             permitResult.deadline,
             permitResult.v,
             permitResult.r,
             permitResult.s,
           ),
-          'WETH: invalid permit',
+          'Dai/invalid-permit',
         )
       })
 
       describe('with a positive allowance', async () => {
         beforeEach(async () => {
-          await dai.approve(signers.user2, 1, { from: signers.user1 })
+          await dai.connect(signers.user1).approve(signers.user2.address, 1)
         })
 
         it('transfers dai using transferFrom and allowance', async () => {
-          const balanceBefore = await dai.balanceOf(signers.user2)
-          await dai.transferFrom(signers.user1, signers.user2, 1, { from: signers.user2 })
-          const balanceAfter = await dai.balanceOf(signers.user2)
-          balanceAfter.toString().should.equal(balanceBefore.add(new BN('1')).toString())
+          const balanceBefore = await dai.balanceOf(signers.user2.address)
+          await dai.connect(signers.user2).transferFrom(signers.user1.address, signers.user2.address, 1)
+          const balanceAfter = await dai.balanceOf(signers.user2.address)
+          balanceAfter.toString().should.equal(balanceBefore.add(1).toString())
         })
 
         it('should not transfer beyond allowance', async () => {
-          await expectRevert(dai.transferFrom(signers.user1, signers.user2, 2, { from: signers.user2 }))
+          await expectRevert.unspecified(dai.connect(signers.user2).transferFrom(signers.user1.address, signers.user2.address, 2))
         })
       })
 
       describe('with a maximum allowance', async () => {
         beforeEach(async () => {
-          await dai.approve(signers.user2, MAX, { from: signers.user1 })
+          await dai.connect(signers.user1).approve(signers.user2.address, MAX)
         })
 
         it('does not decrease allowance using transferFrom', async () => {
-          await dai.transferFrom(signers.user1, signers.user2, 1, { from: signers.user2 })
-          const allowanceAfter = await dai.allowance(signers.user1, signers.user2)
-          allowanceAfter.toString().should.equal(MAX)
-        })
-
-        it('does not decrease allowance using withdrawFrom', async () => {
-          await dai.withdrawFrom(signers.user1, signers.user2, 1, { from: signers.user2 })
-          const allowanceAfter = await dai.allowance(signers.user1, signers.user2)
+          await dai.connect(signers.user2).transferFrom(signers.user1.address, signers.user2.address, 1)
+          const allowanceAfter = await dai.allowance(signers.user1.address, signers.user2.address)
           allowanceAfter.toString().should.equal(MAX)
         })
       })
