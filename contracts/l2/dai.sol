3491a2eafbfd0ad8ef01d0de8e311360df96065e
Sam MacPherson <xylom44@gmail.com>
2021-03-11 07:41:48 -0500

    license

--- contracts/l2/dai.sol
+++ contracts/l2/dai.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 // Copyright (C) 2017, 2018, 2019 dbrock, rain, mrchico
-// Copyright (C) 2021 MakerDAO
+// Copyright (C) 2021 Dai Foundation
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
