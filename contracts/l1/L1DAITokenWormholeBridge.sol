00158b8bd771dc8ab2847c265d3fbfb0e97dd6f0
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-11-09 16:00:52 +0100

    Add prototype of wormhole bridge

---
+++ contracts/l1/L1DAITokenWormholeBridge.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+// @unsupported: ovm
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.7.6;
+
+import {iOVM_L1ERC20Bridge} from "@eth-optimism/contracts/iOVM/bridge/tokens/iOVM_L1ERC20Bridge.sol";
+import {iOVM_L2ERC20Bridge} from "@eth-optimism/contracts/iOVM/bridge/tokens/iOVM_L2ERC20Bridge.sol";
+import {OVM_CrossDomainEnabled} from "@eth-optimism/contracts/libraries/bridge/OVM_CrossDomainEnabled.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+interface TokenLike {
+  function transferFrom(
+    address _from,
+    address _to,
+    uint256 _value
+  ) external returns (bool success);
+}
+
+contract L1DAIWormholeBridge is OVM_CrossDomainEnabled {
+  // --- Auth ---
+  mapping(address => uint256) public wards;
+
+  function rely(address usr) external auth {
+    wards[usr] = 1;
+    emit Rely(usr);
+  }
+
+  function deny(address usr) external auth {
+    wards[usr] = 0;
+    emit Deny(usr);
+  }
+
+  modifier auth() {
+    require(wards[msg.sender] == 1, "L1DAITokenBridge/not-authorized");
+    _;
+  }
+
+  event Rely(address indexed usr);
+  event Deny(address indexed usr);
+
+  address public immutable l1Token;
+  address public immutable l2DAITokenBridge;
+  address public immutable l2Token;
+  address public immutable escrow;
+  uint256 public isOpen = 1;
+  address public immutable wormholeJoin;
+
+  event Closed();
+
+  constructor(
+    address _l1Token,
+    address _l2DAITokenBridge,
+    address _l2Token,
+    address _l1messenger,
+    address _escrow,
+    address _wormholeJoin
+  ) OVM_CrossDomainEnabled(_l1messenger) {
+    wards[msg.sender] = 1;
+    emit Rely(msg.sender);
+
+    l1Token = _l1Token;
+    l2DAITokenBridge = _l2DAITokenBridge;
+    l2Token = _l2Token;
+    escrow = _escrow;
+    wormholeJoin = _wormholeJoin;
+  }
+
+  function close() external auth {
+    isOpen = 0;
+
+    emit Closed();
+  }
+
+  function finalizeFlush(bytes32 targetDomain, uint256 daiToFlush) external {
+    // settle on join
+    // update state root
+    // emit event
+  }
+}
---
+++ contracts/l2/L2DAIWormholeBridge.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity >=0.7.6;
+pragma abicoder v2;
+
+import {iOVM_L1ERC20Bridge} from "@eth-optimism/contracts/iOVM/bridge/tokens/iOVM_L1ERC20Bridge.sol";
+import {iOVM_L2ERC20Bridge} from "@eth-optimism/contracts/iOVM/bridge/tokens/iOVM_L2ERC20Bridge.sol";
+import {OVM_CrossDomainEnabled} from "@eth-optimism/contracts/libraries/bridge/OVM_CrossDomainEnabled.sol";
+
+interface Mintable {
+  function mint(address usr, uint256 wad) external;
+
+  function burn(address usr, uint256 wad) external;
+}
+
+interface IL1WormholeBridge {
+  function finalizeFlush(bytes32 targetDomain, uint256 daiToFlush) external;
+}
+
+struct WormholeGUID {
+  bytes32 sourceDomain;
+  bytes32 targetDomain;
+  address receiver;
+  address operator;
+  uint128 amount;
+  uint64 nonce;
+  uint64 timestamp;
+}
+
+// Mint tokens on L2 after locking funds on L1.
+// Burn tokens on L1 and send a message to unlock tokens on L1 to L1 counterpart
+// Note: when bridge is closed it will still process in progress messages
+
+contract L2DAITokenBridge is OVM_CrossDomainEnabled {
+  // --- Auth ---
+  mapping(address => uint256) public wards;
+
+  function rely(address usr) external auth {
+    wards[usr] = 1;
+    emit Rely(usr);
+  }
+
+  function deny(address usr) external auth {
+    wards[usr] = 0;
+    emit Deny(usr);
+  }
+
+  modifier auth() {
+    require(wards[msg.sender] == 1, "L2DAITokenBridge/not-authorized");
+    _;
+  }
+
+  event Rely(address indexed usr);
+  event Deny(address indexed usr);
+
+  address public immutable l1Token;
+  address public immutable l2Token;
+  address public immutable l1DAITokenBridge;
+  bytes32 public immutable sourceDomain;
+  uint256 public isOpen = 1;
+  uint64 public wormholeId = 0;
+  mapping(bytes32 => bool) public wormholes;
+  mapping(bytes32 => uint256) public batchedDaiToFlush;
+
+  event Closed();
+
+  event WormholeInitialized(WormholeGUID wormhole);
+  event Flushed(bytes32 targetDomain, uint256 dai);
+
+  constructor(
+    address _l2CrossDomainMessenger,
+    address _l2Token,
+    address _l1Token,
+    address _l1DAITokenBridge,
+    bytes32 _sourceDomain
+  ) public OVM_CrossDomainEnabled(_l2CrossDomainMessenger) {
+    wards[msg.sender] = 1;
+    emit Rely(msg.sender);
+
+    l2Token = _l2Token;
+    l1Token = _l1Token;
+    l1DAITokenBridge = _l1DAITokenBridge;
+    sourceDomain = _sourceDomain;
+  }
+
+  function close() external auth {
+    isOpen = 0;
+
+    emit Closed();
+  }
+
+  function initiateWormhole(
+    bytes32 targetDomain,
+    address receiver,
+    uint128 amount,
+    address operator
+  ) external {
+    WormholeGUID memory wormhole = WormholeGUID({
+      sourceDomain: sourceDomain,
+      targetDomain: targetDomain,
+      receiver: receiver,
+      operator: operator,
+      amount: amount,
+      nonce: wormholeId++,
+      timestamp: uint64(block.timestamp)
+    });
+    bytes32 wormholeHash = getWormholeHash(wormhole);
+
+    wormholes[wormholeHash] = true;
+    batchedDaiToFlush[targetDomain] += amount;
+    Mintable(l2Token).burn(msg.sender, amount);
+
+    emit WormholeInitialized(wormhole);
+  }
+
+  function flush(bytes32 targetDomain) external {
+    uint32 l1Gas = 20000; // @todo: i think this should be hardcoded to avoid griefing
+    uint256 daiToFlush = batchedDaiToFlush[targetDomain];
+
+    bytes memory message = abi.encodeWithSelector(
+      IL1WormholeBridge.finalizeFlush.selector,
+      targetDomain,
+      daiToFlush
+    );
+    sendCrossDomainMessage(l1DAITokenBridge, l1Gas, message);
+
+    batchedDaiToFlush[targetDomain] = 0;
+    emit Flushed(targetDomain, daiToFlush);
+  }
+
+  function getWormholeHash(WormholeGUID memory wormhole) public pure returns (bytes32) {
+    return
+      keccak256(
+        abi.encode(
+          wormhole.sourceDomain,
+          wormhole.targetDomain,
+          wormhole.receiver,
+          wormhole.operator,
+          wormhole.amount,
+          wormhole.nonce,
+          wormhole.timestamp
+        )
+      );
+  }
+}
